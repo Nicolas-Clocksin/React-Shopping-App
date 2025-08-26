1adf863624667c9d05f2e5a090f159daf6d73e5e
nicolas-clocksin-ASU <60439673+nicolas-clocksin-ASU@users.noreply.github.com>
2025-08-26 12:11:36 -0700

    feat: added scrollability to the list items

--- React-Shopping-App/src/components/CategoryList.jsx
+++ React-Shopping-App/src/components/CategoryList.jsx
@@ -8,24 +8,26 @@ function CategoryList({categories, items}){
           {categories.map((category) => (
             <div key={category.id}>
               <h1>{category.name}</h1>
-              <ul className="d-flex overflow-auto gap-3 px-1 list-unstyled m-0">
-                {items
-                  .filter((item) => item.category === category.id)
-                  .map((item) => (
-                    <li key={item.id}>
-                        <Card style={{ width: '18rem', height: '315px'}}>
-                            <Card.Img style={{maxHeight: '180px', objectFit: 'contain'}} variant="top" src={item.imgUrl} />
-                            <CardBody className="card-body">
-                                <Card.Title>{item.name}</Card.Title>
-                                <Card.Text>${item.price} USD</Card.Text>
-                              <div>
-                                <Button size="sm">Add to Cart</Button>
-                                </div>
-                            </CardBody>
-                        </Card>
-                    </li>
-                  ))}
-              </ul>
+              <div className="categoryItems">
+                <ul className="d-flex overflow-auto gap-3 px-1 list-unstyled m-0">
+                  {items
+                    .filter((item) => item.category === category.id)
+                    .map((item) => (
+                      <li key={item.id}>
+                          <Card style={{ width: '18rem', height: '315px'}}>
+                              <Card.Img style={{maxHeight: '180px', objectFit: 'contain'}} variant="top" src={item.imgUrl} />
+                              <CardBody className="card-body">
+                                  <Card.Title>{item.name}</Card.Title>
+                                  <Card.Text>${item.price} USD</Card.Text>
+                                <div>
+                                  <Button size="sm">Add to Cart</Button>
+                                  </div>
+                              </CardBody>
+                          </Card>
+                      </li>
+                    ))}
+                </ul>
+              </div>
             </div>
           ))}
         </div>
