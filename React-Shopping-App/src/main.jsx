f63c22155b3d05f8fee517651649cd7339fb4f8d
nicolas-clocksin-ASU <60439673+nicolas-clocksin-ASU@users.noreply.github.com>
2025-09-02 12:39:03 -0700

    feat: added routing to main

--- React-Shopping-App/src/main.jsx
+++ React-Shopping-App/src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Login from './components/Login.jsx'
+import { createBrowserRouter, RouterProvider }  from 'react-router-dom'
+import NotFoundPage from './pages/NotFoundPage.jsx'
+import ItemPage from './pages/ItemPage.jsx'
+import HomePage from './pages/HomePage.jsx'
+import ProtectedRoute from './auth/ProtectedRoute.jsx'
 
+const router = createBrowserRouter([
+  {path: "/login", element: <Login />},
+ 
+  {
+    element: <ProtectedRoute />,
+    children: [
+      {path: "/home", element: <HomePage />},
+      {path: "/item/:id", element: <ItemPage />},
+      {path: "*", element: <NotFoundPage />},
+    ]
+  }
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
