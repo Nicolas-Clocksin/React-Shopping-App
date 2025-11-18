24060968ac4bde9da778809faf5c36be3ceb64c8
nicolas-clocksin-ASU <60439673+nicolas-clocksin-ASU@users.noreply.github.com>
2025-11-18 13:08:31 -0700

    fix: updated so that when quantity of a same item is added to cart, it is updated in the cart.

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { useAuth } from "./AuthContext";
 
 export const CartContext = createContext({
   cartItems: [],
@@ -6,16 +7,28 @@ export const CartContext = createContext({
 });
 
 export function CartProvider({ children }) {
+  const { user } = useAuth();
   const [cartItems, setCartItems] = useState([]);
   const addToCart = (item, quantity) => {
-    setCartItems((prev) => [
-      ...prev,
-      {
-        item,
-        quantity,
-        added: true,
-      },
-    ]);
+    if (cartItems.find((cartItem) => cartItem.item.id === item.id)) {
+      setCartItems((prev) =>
+        prev.map((cartItem) =>
+          cartItem.item.id === item.id
+            ? { ...cartItem, quantity: cartItem.quantity + quantity }
+            : cartItem
+        )
+      );
+    } else {
+      setCartItems((prev) => [
+        ...prev,
+        {
+          item,
+          quantity,
+          added: true,
+          user: user,
+        },
+      ]);
+    }
   };
   const cartQuantityTotal = cartItems.reduce(
     (sum, cartEntry) => sum + Number(cartEntry.quantity || 0),
