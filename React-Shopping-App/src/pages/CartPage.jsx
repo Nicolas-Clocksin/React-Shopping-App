f1ea90ebaf733595b63c6b1b33988ca7b5abde7f
nicolas-clocksin-ASU <60439673+nicolas-clocksin-ASU@users.noreply.github.com>
2025-11-17 17:01:53 -0700

    feat: created default page to list items added to cart

--- React-Shopping-App/src/pages/CartPage.jsx
+++ React-Shopping-App/src/pages/CartPage.jsx
@@ -1,7 +1,15 @@
+import React, {useState, useContext} from "react";
+import { CartContext } from "../context/CartContext";
 function CartPage() {
+    const { cartItems } = useContext(CartContext);
     return(
         <div>
             <h1>Cart Page</h1>
+            <ul>
+            {cartItems.map((cartItem)=>(
+                <li>{cartItem.name}</li>
+            ))}
+            </ul>
         </div>
     );
 }
