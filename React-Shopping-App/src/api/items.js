2e6e0a48cc515c244ec0ada19d2d3e35ad2ccd8a
nicolas-clocksin-ASU <60439673+nicolas-clocksin-ASU@users.noreply.github.com>
2025-11-17 21:02:50 -0700

    fix: updated pricing on items

--- React-Shopping-App/src/api/items.js
+++ React-Shopping-App/src/api/items.js
@@ -14,7 +14,7 @@ export async function ItemList(){
                 name: 'Game',
                 description: 'Video Game',
                 imgUrl: 'https://store-images.s-microsoft.com/image/apps.808.14492077886571533.be42f4bd-887b-4430-8ed0-622341b4d2b0.c8274c53-105e-478b-9f4b-41b8088210a3?q=90&w=177&h=265',
-                price: 30
+                price: 40
               },
               {
                 id: 3,
@@ -22,7 +22,7 @@ export async function ItemList(){
                 name: 'Game',
                 description: 'Video Game',
                 imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/God_of_War_4_cover.jpg/250px-God_of_War_4_cover.jpg',
-                price: 30
+                price: 60
               },
               {  
                 id: 4,
@@ -30,7 +30,7 @@ export async function ItemList(){
                 name: 'Random Toys',
                 description: 'this is a toy',
                 imgUrl: 'https://www.chemicalsafetyfacts.org/wp-content/uploads/shutterstock_383521510-002-scaled.jpg',
-                price: 30
+                price: 70
               },
               {
                 id: 5,
@@ -38,7 +38,7 @@ export async function ItemList(){
                 name: 'Random Toys',
                 description: 'this is a toy',
                 imgUrl: 'https://www.goodfishgroup.com/wp-content/uploads/2024/07/AdobeStock_49843072-2048x1371.jpeg',
-                price: 30
+                price: 50
               },
               {
                 id: 6,
@@ -46,7 +46,7 @@ export async function ItemList(){
                 name: 'Game',
                 description: 'this is a toy',
                 imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/God_of_War_4_cover.jpg/250px-God_of_War_4_cover.jpg',
-                price: 30
+                price: 20
               },
               {  
                 id: 7,
@@ -54,7 +54,7 @@ export async function ItemList(){
                 name: 'Toy',
                 description: 'this is a toy',
                 imgUrl: 'https://i.imgur.com/eOtEAB7.jpg',
-                price: 30
+                price: 10
               },
               {
                 id: 8,
@@ -62,7 +62,7 @@ export async function ItemList(){
                 name: 'Game',
                 description: 'this is a toy',
                 imgUrl: 'https://store-images.s-microsoft.com/image/apps.808.14492077886571533.be42f4bd-887b-4430-8ed0-622341b4d2b0.c8274c53-105e-478b-9f4b-41b8088210a3?q=90&w=177&h=265',
-                price: 30
+                price: 90
               },
               {
                 id: 9,
@@ -70,7 +70,7 @@ export async function ItemList(){
                 name: 'Game',
                 description: 'this is a toy',
                 imgUrl: 'https://i.pinimg.com/236x/be/42/77/be4277c296c2d7c0b5f682d8c03219ab.jpg',
-                price: 30
+                price: 40
               }
               , {  
                 id: 10,
@@ -78,7 +78,7 @@ export async function ItemList(){
                 name: 'Toy',
                 description: 'this is a toy',
                 imgUrl: 'https://media.istockphoto.com/id/909772478/photo/brown-teddy-bear-isolated-in-front-of-a-white-background.jpg?s=612x612&w=0&k=20&c=F4252bOrMfRTB8kWm2oM2jlb9JXY08tKCaO5G_ms1Uw=',
-                price: 30
+                price: 70
               },
               {
                 id: 11,
@@ -86,7 +86,7 @@ export async function ItemList(){
                 name: 'Game',
                 description: 'this is a toy',
                 imgUrl: 'https://store-images.s-microsoft.com/image/apps.808.14492077886571533.be42f4bd-887b-4430-8ed0-622341b4d2b0.c8274c53-105e-478b-9f4b-41b8088210a3?q=90&w=177&h=265',
-                price: 30
+                price: 10
               },
               {
                 id: 12,
@@ -94,7 +94,7 @@ export async function ItemList(){
                 name: 'Game',
                 description: 'this is a toy',
                 imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/God_of_War_4_cover.jpg/250px-God_of_War_4_cover.jpg',
-                price: 30
+                price: 50
               }
     ]
 }
