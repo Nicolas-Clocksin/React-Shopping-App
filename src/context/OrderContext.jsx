08b2ba0de5691bb965533c9d415c33ffaa754151
nicolas-clocksin-ASU <60439673+nicolas-clocksin-ASU@users.noreply.github.com>
2025-11-18 16:44:44 -0700

    feat: added routing and context for creating an order

---
+++ src/context/OrderContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState } from "react";
+export const OrderContext = createContext({});
+
+export function OrderProvider({ children }) {
+  const [order, setOrder] = useState(null);
+  const [orders, setOrders] = useState([]);
+  const { user } = useAuth();
+  function createOrder(address, paymentMethod, cartItems, totalAmount) {
+    setOrder({
+      userId: user.id,
+      address,
+      paymentMethod,
+      items: cartItems,
+      totalAmount,
+      date: new Date().toISOString(),
+    });
+    addToOrders(order);
+  }
+  function addToOrders(newOrder) {
+    setOrders((prevOrders) => [...prevOrders, newOrder]);
+  }
+  return (
+    <OrderContext.Provider value={{ order, createOrder }}>
+      {children}
+    </OrderContext.Provider>
+  );
+}
