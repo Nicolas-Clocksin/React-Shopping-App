8d1023069454e73e34f86469f5fb086b89575c99
Nicolas-Clocksin <nac1998@cox.net>
2025-11-20 16:19:38 -0700

    feat: update to shipping form to have two types billing and shipping for checkout

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -2,14 +2,19 @@ import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
 import { OrderContext } from "../context/OrderContext";
 import ShippingForm from "../components/ShippingForm";
+import Button from "react-bootstrap/Button";
 
 function CheckoutPage() {
   const { cartItems, totalAmount } = useContext(CartContext);
   const { createOrder } = useContext(OrderContext);
 
   return (
-    <div className="checkoutPage">
-      <ShippingForm />
+    <div className="checkoutPage d-flex flex-column gap-3 justify-content-center align-items-center">
+      <ShippingForm typeShipment="shipping" />
+      <ShippingForm typeShipment="billing" />
+      <div className="text-center">
+        <Button type="submit">Submit</Button>
+      </div>
     </div>
   );
 }
