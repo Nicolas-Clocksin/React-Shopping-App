b92b7772b66a21c22a8e497ce9f2edabd807cdda
Nicolas-Clocksin <nac1998@cox.net>
2025-11-20 17:05:19 -0700

    feat: updated checkout page to be able to hide/show billing information. Added summary of order to the right hand side.

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,19 +1,23 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "../context/CartContext";
 import { OrderContext } from "../context/OrderContext";
 import ShippingForm from "../components/ShippingForm";
 import Button from "react-bootstrap/Button";
-
+import OrderSummary from "../components/OrderSummary";
 function CheckoutPage() {
   const { cartItems, totalAmount } = useContext(CartContext);
   const { createOrder } = useContext(OrderContext);
-
+  const [diffBilling, setDiffBilling] = useState(false);
   return (
-    <div className="checkoutPage d-flex flex-column gap-3 justify-content-center align-items-center">
-      <ShippingForm typeShipment="shipping" />
-      <ShippingForm typeShipment="billing" />
-      <div className="text-center">
-        <Button type="submit">Submit</Button>
+    <div className="container py-4">
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-8 d-flex flex-column gap-3">
+          <ShippingForm typeShipment="shipping" />
+          {diffBilling ? <ShippingForm typeShipment="billing" /> : null}
+        </div>
+        <div className="col-12 col-md-4">
+          <OrderSummary cartItems={cartItems} totalAmount={totalAmount} />
+        </div>
       </div>
     </div>
   );
