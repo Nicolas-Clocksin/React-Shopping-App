e0b8d7eeef44584046ab38bcd223c09b98f84c85
Nicolas-Clocksin <nac1998@cox.net>
2025-11-25 09:08:43 -0700

    doc: authorization component

--- src/auth/ProtectedRoute.jsx
+++ src/auth/ProtectedRoute.jsx
@@ -1,8 +1,8 @@
 /*
-  Title:
+  Title: ProtectedRoute
   Created By: Nicolas Clocksin
 
-  Description: 
+  Description: A component that protects routes that require authentication.
 */
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
