6fdc44282fcf6af8bd69e164f32e82ec12245ab3
Nicolas-Clocksin <nac1998@cox.net>
2026-01-06 11:47:31 -0700

    fix: updated address context to be able to handle changes from user input and store

--- src/context/AddressContext.jsx
+++ src/context/AddressContext.jsx
@@ -10,11 +10,11 @@ import { createContext, useState } from "react";
 export const AddressConext = createContext({});
 
 export function AddressProvider({ children }) {
-  const { street, setStreet } = useState("");
-  const { city, setCity } = useState("");
-  const { postalCode, setPostalCode } = useState("");
-  const { state, setState } = useState("");
-  const { address, setAddress } = useState([]);
+  const [street, setStreet] = useState("");
+  const [city, setCity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [state, setState] = useState("");
+  const [address, setAddress] = useState([]);
 
   function updateStreet(event) {
     setStreet(event.target.value);
@@ -25,15 +25,21 @@ export function AddressProvider({ children }) {
   function updatePostalCode(event) {
     setPostalCode(event.target.value);
   }
-  function updateState(event) {
-    setState(event.target.value);
+  function updateState(value) {
+    setState(value);
   }
   function addAddress() {
-    setAddress([street, city, postalCode, state]);
+    const newAddress = { street, city, postalCode, state };
+    setAddress(newAddress);
+    return newAddress;
   }
   return (
     <AddressConext.Provider
       value={{
+        street,
+        city,
+        postalCode,
+        state,
         updateStreet,
         updateCity,
         updatePostalCode,
