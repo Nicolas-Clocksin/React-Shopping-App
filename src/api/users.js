754fe1679bbea0313f2c60b3fa17e900f54a0761
Nicolas-Clocksin <nac1998@cox.net>
2026-01-06 09:49:37 -0700

    update: added default cart items for test users

--- src/api/users.js
+++ src/api/users.js
@@ -13,7 +13,33 @@ export async function UserList() {
       email: "email@email.com",
       password: "test",
       orders: [],
-      cart: [],
+      cart: [
+        {
+          item: {
+            id: 1,
+            name: "Teddy Bear",
+            price: 30,
+            imgUrl:
+              "https://media.istockphoto.com/id/909772478/photo/brown-teddy-bear-isolated-in-front-of-a-white-background.jpg?s=612x612&w=0&k=20&c=F4252bOrMfRTB8kWm2oM2jlb9JXY08tKCaO5G_ms1Uw=",
+            price: 30,
+            category: 1,
+            description: "this is a toy",
+          },
+          quantity: 2,
+        },
+        {
+          item: {
+            id: 3,
+            name: "Game",
+            price: 60,
+            imgUrl:
+              "https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/God_of_War_4_cover.jpg/250px-God_of_War_4_cover.jpg",
+            category: 2,
+            description: "Video Game",
+          },
+          quantity: 1,
+        },
+      ],
       paymentMethod: [],
       addresses: [],
     },
@@ -24,7 +50,20 @@ export async function UserList() {
       email: "test@test.com",
       password: "test",
       orders: [],
-      cart: [],
+      cart: [
+        {
+          item: {
+            id: 2,
+            name: "Game",
+            price: 40,
+            imgUrl:
+              "https://store-images.s-microsoft.com/image/apps.808.14492077886571533.be42f4bd-887b-4430-8ed0-622341b4d2b0.c8274c53-105e-478b-9f4b-41b8088210a3?q=90&w=177&h=265",
+            category: 2,
+            description: "Video Game",
+          },
+          quantity: 1,
+        },
+      ],
       paymentMethod: [],
       addresses: [],
     },
