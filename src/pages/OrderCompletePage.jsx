1edc4eec5e8dde154c176fd8382b27b7e0d5458a
Nicolas-Clocksin <nac1998@cox.net>
2026-01-06 15:27:38 -0700

    fix: remove unused user auth

--- src/pages/OrderCompletePage.jsx
+++ src/pages/OrderCompletePage.jsx
@@ -4,7 +4,6 @@ import { OrderContext } from "../context/OrderContext";
 import { useAuth } from "../context/AuthContext";
 function OrderCompletePage() {
   const { order, userOrders } = useContext(OrderContext);
-  const { user } = useAuth();
 
   useEffect(() => {
     console.log("Order Complete Page - User orders:", userOrders);
