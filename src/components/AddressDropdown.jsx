d87bb850f29858415c298117fc0eec6651c957b1
Nicolas-Clocksin <nac1998@cox.net>
2026-01-09 09:25:16 -0700

    doc: documentation for address/payment dropdown

--- src/components/AddressDropdown.jsx
+++ src/components/AddressDropdown.jsx
@@ -1,20 +1,25 @@
+/*
+  Title: AddressDropdown.jsx
+  Created By: Nicolas Clocksin
+
+  Description: AddressDropdown is used to display a dropdown of stored addrreses for the
+  user on checkout.
+*/
 import React from "react";
 import { useContext } from "react";
 import { useAuth } from "../context/AuthContext";
 import { AddressConext } from "../context/AddressContext";
 import { Button, Form } from "react-bootstrap";
 function AddressDropdown({ setShowAddressDropdown }) {
-  const {
-    addresses,
-    selectedAddressIndex,
-    updateSelectedAddress,
-  } = useContext(AddressConext);
+  // Get addresses and selected address index from AddressContext
+  const { addresses, selectedAddressIndex, updateSelectedAddress } =
+    useContext(AddressConext);
   const { user } = useAuth();
-
+  // Validate if use/addresses exist
   if (!user || !addresses || addresses.length === 0) {
     return null;
   }
-
+  // Return dropdown form of the addresses
   return (
     <Form className="d-flex flex-column gap-2">
       <Form.Group>
