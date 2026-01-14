2656732546a00756d2501bcd32b6036a9735ff86
Nicolas-Clocksin <nac1998@cox.net>
2026-01-14 13:09:36 -0700

    feat: set up to handle changes for edit modal

--- src/modals/EditProfileModal.jsx
+++ src/modals/EditProfileModal.jsx
@@ -4,12 +4,27 @@ import { useAuth } from "../context/AuthContext";
 
 function EditProfileModal() {
   const [show, setShow] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   function handleShow() {
     setShow(true);
   }
   function handleClose() {
     setShow(false);
   }
+  const updateName = (event) => {
+    setName(event.target.value);
+  };
+  const updateEmail = (event) => {
+    setEmail(event.target.value);
+  };
+  const updatePassword = (event) => {
+    setPassword(event.target.value);
+  };
+  const updateProfile = () => {
+    setShow(false);
+  };
   const { user, setUser } = useAuth();
   return (
     <div>
@@ -22,13 +37,18 @@ function EditProfileModal() {
           <Form>
             <Form.Group>
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder={user.name}></Form.Control>
+              <Form.Control
+                type="text"
+                placeholder={user.name}
+                onChange={(event) => updateName(event)}
+              ></Form.Control>
             </Form.Group>
             <Form.Group>
               <Form.Label>Email</Form.Label>
               <Form.Control
                 type="email"
                 placeholder={user.email}
+                onChange={(event) => updateEmail(event)}
               ></Form.Control>
             </Form.Group>
             <Form.Group>
@@ -36,12 +56,13 @@ function EditProfileModal() {
               <Form.Control
                 type="password"
                 placeholder={user.password}
+                onChange={(event) => updatePassword(event)}
               ></Form.Control>
             </Form.Group>
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button>Update Profile</Button>
+          <Button onClick={updateProfile}>Update Profile</Button>
           <Button onClick={handleClose}>Cancel</Button>
         </Modal.Footer>
       </Modal>
